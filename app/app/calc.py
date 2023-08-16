43cd02567a6e06fad336eb118e3f0e1725fa5d75
Jay Dee <jay_gabrillo@cody.inc>
2023-08-16 17:24:00 +0800

    database migrations

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """ adding x and y returns result """
+    return x + y
+
+def subtract(x, y):
+    """ subtracting x and y returns result"""
+    return x - y
