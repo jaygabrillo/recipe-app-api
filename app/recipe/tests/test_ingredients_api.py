42948a8ea7b2b7e9adc1b880cd80d3e2f26f31df
Jay Dee <jay_gabrillo@cody.inc>
2023-08-23 16:32:43 +0800

    implement ingredient listing api

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -64,5 +64,5 @@ class PrivateIngredientsApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
-        self.assertEqual(res.data[0]['nmame'], ingredient.name)
+        self.assertEqual(res.data[0]['name'], ingredient.name)
         self.assertEqual(res.data[0]['id'], ingredient.id)
